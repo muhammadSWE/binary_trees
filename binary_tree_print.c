7794f65b8f1296746941c346c12088ff86fdce1d
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-03-28 14:47:34 +0200

    Add binary tree print functions to .h

--- binary_tree_print.c
+++ binary_tree_print.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include "binary_trees.h"
+
 /* Original code from http://stackoverflow.com/a/13755911/5184480 */
 
 /**
-* print_t - Stores recursively each level in an array of strings
-*
-* @tree: Pointer to the node to print
-* @offset: Offset to print
-* @depth: Depth of the node
-* @s: Buffer
-*
-* Return: length of printed tree after process
-*/
+ * print_t - Stores recursively each level in an array of strings
+ *
+ * @tree: Pointer to the node to print
+ * @offset: Offset to print
+ * @depth: Depth of the node
+ * @s: Buffer
+ *
+ * Return: length of printed tree after process
+ */
 static int print_t(const binary_tree_t *tree, int offset, int depth, char **s)
 {
 	char b[6];
